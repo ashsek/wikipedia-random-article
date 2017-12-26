1ccf8733bb9834e640a247d35f74f80154b9f150
Ashwin Sekhari <ashwinsekhari@gmail.com>
2017-12-26 11:49:06 +0530

    Added favourite function

--- generate.py
+++ generate.py
@@ -16,6 +16,8 @@ def randomgenerate():
     To display and open a random wikipedia article 
     
     '''
+    global url3
+    global Title
     url = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=json'
     url2 = 'https://en.wikipedia.org/wiki?curid='
     print('===========================================')
@@ -43,6 +45,7 @@ def randomgenerate():
             print('---------------------------------------------------------')
             print(' If you want to refresh the list of articles type refresh')
             print('Or you can quit the generator by typing quit')
+            print('You can favourite this article for later refrence :)')
             print('---------------------------------------------------------')
         while k < j:
             print(js["query"]['random'][k]['title'])
@@ -52,7 +55,11 @@ def randomgenerate():
         
         if y.lower() == 'yes':
             print('opening browser....')
-            webbrowser.open(url2 + str(article_id), new=2)
+            url3 = url2 + str(article_id)
+            webbrowser.open(url3, new=2)
+            fav = input(" Do you want to add this item to your favourites:")
+            if fav == 'yes':
+                favourite()                
             break
         elif y.lower() == 'refresh':
             refresh()
@@ -66,3 +73,10 @@ def refresh():
     To refresh the list of articles
     '''
     randomgenerate()
+
+def favourite():
+    global url3
+    global Title
+    fh = open('fav_articles.txt', 'a+')
+    fh.write(Title + ' : ' +  url3 + '\r\n')
+    fh.close()
