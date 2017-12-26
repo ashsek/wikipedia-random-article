b9d91a714473477a246b4ae201a80a28da5d8605
Ashwin Sekhari <ashwinsekhari@gmail.com>
2017-12-26 11:35:50 +0530

    add Quit and Refresh options

--- generate.py
+++ generate.py
@@ -11,6 +11,11 @@ import json
 import webbrowser
 
 def randomgenerate():
+    '''
+    
+    To display and open a random wikipedia article 
+    
+    '''
     url = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=json'
     url2 = 'https://en.wikipedia.org/wiki?curid='
     print('===========================================')
@@ -35,15 +40,29 @@ def randomgenerate():
         article_id = js["query"]['random'][i]['id']
         if k == 0:
             print('List of articles in line:')
+            print('---------------------------------------------------------')
+            print(' If you want to refresh the list of articles type refresh')
+            print('Or you can quit the generator by typing quit')
+            print('---------------------------------------------------------')
         while k < j:
             print(js["query"]['random'][k]['title'])
             k += 1
-        print('--------------------------------------')
+        
         y = input('Do you want to read about '+ Title + ':')
         
         if y.lower() == 'yes':
             print('opening browser....')
             webbrowser.open(url2 + str(article_id), new=2)
             break
+        elif y.lower() == 'refresh':
+            refresh()
+        elif y.lower() == 'quit':
+            break
         else:
             i += 1
+
+def refresh():
+    '''
+    To refresh the list of articles
+    '''
+    randomgenerate()
